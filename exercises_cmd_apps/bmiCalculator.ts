7826355c93c2d770a73f396004900f72ecb5fb13
eliasker <eliasker@gmail.com>
2020-10-02 00:36:06 +0300

    BMI & daily exercise calculators on commandline (exercises 1-3)

---
+++ exercises_cmd_apps/bmiCalculator.ts
@@ -0,0 +1,39 @@
+interface BmiValues {
+  height: number,
+  weight: number,
+}
+
+const parseBmiArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / Math.pow(height / 100, 2);
+
+  if (bmi < 15) return 'Very severely underweight';
+  else if (bmi < 16) return 'Severely underweight';
+  else if (bmi < 18.5) return 'Underweight';
+  else if (bmi < 25) return 'Normal (healthy weight)';
+  else if (bmi < 30) return 'Overweight';
+  else if (bmi < 35) return 'Obese Class I (Moderately obese)';
+  else if (bmi < 40) return 'Obese Class II (Severely obses)';
+  else return 'Obese Class III (Very severely obese)'
+
+}
+
+try {
+  const { height, weight } = parseBmiArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (e) {
+  console.log('Error, something went wrong, message: ', e.message);
+}
---
+++ exercises_cmd_apps/exerciseCalculator.ts
@@ -0,0 +1,68 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number,
+  target: number,
+  average: number,
+  success: boolean,
+  rating: number,
+  ratingDescription: string
+}
+
+interface ExerciseValues {
+  trainingPerDay: Array<number>,
+  target: number
+}
+
+const parseExerciseArguments = (args: Array<string>): ExerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  let trainingPerDay: Array<number> = [];
+
+  for (let i = 2; i < args.length - 1; i++) {
+    if (isNaN(Number(args[i]))) throw new Error('Provided values were not numbers!');
+    trainingPerDay.push(Number(args[i]));
+  }
+  if (isNaN(Number(args[args.length - 1]))) throw new Error('Provided values were not numbers!');
+  return {
+    trainingPerDay: trainingPerDay, target: Number(args[args.length - 1]),
+  }
+}
+
+
+/**
+ * Calculates average time of daily exercise hours and returns feedback 
+ * @param trainingPerDay Number of training hours per day
+ * @param target Target amount of daily hours
+ * @returns Result object that describes performance based on target and done
+ */
+const calculateExercise = (trainingPerDay: Array<number>, target: number): Result => {
+  const totalTraining = trainingPerDay.reduce((a, b) => a + b, 0);
+  const averageTrainingPerDay = totalTraining / trainingPerDay.length;
+  let givenRating = 0;
+  let givenDesc = '';
+  if (averageTrainingPerDay > target + 0.5) {
+    givenRating = 3;
+    givenDesc = 'Great job!';
+  } else if (averageTrainingPerDay > target - 0.5) {
+    givenRating = 2;
+    givenDesc = 'You did good';
+  } else {
+    givenRating = 1;
+    givenDesc = 'You can do better!';
+  }
+  return {
+    periodLength: trainingPerDay.length,
+    trainingDays: trainingPerDay.filter(t => t !== 0).length,
+    success: averageTrainingPerDay >= 1,
+    target: target,
+    average: averageTrainingPerDay,
+    rating: givenRating,
+    ratingDescription: givenDesc,
+  }
+}
+
+try {
+  const { trainingPerDay, target } = parseExerciseArguments(process.argv);
+  console.log(calculateExercise(trainingPerDay, target));
+} catch (e) {
+  console.log('Error, something went wrong, message: ', e.message);
+}
