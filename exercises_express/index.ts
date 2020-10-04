f61b321a782ebac30bb5455d04f556d3ef5a033c
eliasker <eliasker@gmail.com>
2020-10-04 15:25:52 +0300

    combined exercise endpoints to index.ts

--- exercises_express/index.ts
+++ exercises_express/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 const app = express();
 
-import { calculateBmi } from '../exercises_cmd_apps/bmiCalculator'
+import { calculateBmi } from '../exercises_cmd_apps/bmiCalculator';
+
+app.get('/', (_req, res) => {
+  res.send('Check');
+});
 
 // bmi?height=180&weight=72
 app.get('/bmi', (_req, res) => {
-  const { height, weight } = _req.query
+  const { height, weight } = _req.query;
   console.log(height, weight, _req.query);
   if (isNaN(Number(height)) && isNaN(Number(weight))) {
-    res.json('malformatted parameters')
+    res.json('malformatted parameters');
   } else {
     try {
       const bmi = calculateBmi(Number(height), Number(weight));
@@ -16,15 +20,19 @@ app.get('/bmi', (_req, res) => {
         height: height,
         weight: weight,
         bmi: bmi
-      })
+      });
     } catch (e) {
-      res.status(500).json({ error: 'message' })
+      res.status(500).json({ error: 'message' });
     }
   }
+});
+
+app.get('/exercises', (_req, res) => {
+  res.send('hello from exercises');
 });
 
 const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
